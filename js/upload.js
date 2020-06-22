b44ba82d4c1abd7f3e06316a7fc907198e80e4ca
Mollycodd1e <selik7@mail.ru>
2020-06-22 16:32:43 +0300

    Добавление upload файла

---
+++ js/upload.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function () {
+  var URL = 'https://javascript.pages.academy/code-and-magick';
+
+  xhr.open = ('GET', 'https://javascript.pages.academy/code-and-magick')
+  window.upload = function (data, onSuccess) {
+  };
+})();
